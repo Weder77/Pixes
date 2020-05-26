092ec2b470017a7a4bacf96e4415e304a160f55c
Sheraw <theogrelet05@gmail.com>
2020-05-26 14:38:44 +0200

    :rocket: add all fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,15 +4,34 @@ namespace App\DataFixtures;
 
 use App\Entity\Code;
 use App\Entity\Game;
+use App\Entity\Opinion;
 use App\Entity\Platform;
+use App\Entity\Profile;
 use App\Entity\Tag;
+use App\Entity\User;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Constraints\Date;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        function generateRandomString($length = 4) {
+            return substr(
+                str_shuffle(
+                    str_repeat($x='0123456789ABCDEFGHIJKLMN0PQRSTUVWXYZ', 
+                    ceil($length/strlen($x)) )
+                ),1,$length);
+        }
+
         // Create platforms
         $platforms = [];
         for ($i = 0; $i < 4; $i++) { 
@@ -35,6 +54,7 @@ class AppFixtures extends Fixture
 
         // Create Games
         $pegis = [3, 7, 12, 16, 18];
+        $games = [];
         for ($i=0; $i < 10; $i++) { 
             $game = new Game();
             $game->setName('Game ' . $i);
@@ -49,7 +69,51 @@ class AppFixtures extends Fixture
             for ($x=0; $x < rand(1, 3); $x++) { 
                 $game->addTag($tags[rand(0, sizeof($tags) - 1)]);
             }
+            array_push($games, $game);
             $manager->persist($game);
+        }
+
+        // Generate Codes
+        for ($i=0; $i < 30; $i++) { 
+            $code = new Code();
+            $code->setCode(
+                generateRandomString() . '-' . generateRandomString() . '-' . generateRandomString() . '-' . generateRandomString()
+            );
+            $code->setGame($games[rand(0, 9)]);
+            $code->setUsed(false);
+            $manager->persist($code);
+        }
+
+        // Create Users & Profiles
+        $users = [];
+        for ($i = 1; $i <= 3; $i++) {
+            $user = new User();
+            $user->setEmail('user' . $i . '@test.com');
+            $password = $this->encoder->encodePassword($user, 'User' . $i);
+            $user->setPassword($password);
+            $profile = new Profile();
+            $profile->setFirstname('User');
+            $profile->setLastname($i);
+            $date = new DateTime();
+            $profile->setBirthday($date);
+            $profile->setRegisterDate($date);
+            $profile->setBalance(rand(0,150));
+            $profile->setUser($user);
+            array_push($users, $profile);
+            $manager->persist($user);
+            $manager->persist($profile);
+        }
+
+        // Generate Opinions
+        for ($i=0; $i < 13; $i++) { 
+            $opinion = new Opinion();
+            $opinion->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit. ');
+            $opinion->setNote(rand(0.5,5));
+            $date = new DateTime();
+            $opinion->setPostedOn($date);
+            $opinion->setGame($games[rand(0,9)]);
+            $opinion->setUser($users[rand(0,2)]);
+            $manager->persist($opinion);
         }
 
         $manager->flush();
